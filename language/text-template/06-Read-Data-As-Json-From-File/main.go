38bd50124b4c46b357c9a56ff1e8a149f94d5865
KoderDojo <KoderDojo@users.noreply.github.com>
2017-08-06 15:35:12 -0600

    Add text/template example.

---
+++ language/text-template/06-Read-Data-As-Json-From-File/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	var data struct {
+		Name    string   `json:"name"`
+		Age     int      `json:"age"`
+		Hobbies []string `json:"hobbies"`
+	}
+
+	dataFile, err := os.Open("data2.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonParser := json.NewDecoder(dataFile)
+	if err = jsonParser.Decode(&data); err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl, err := template.ParseFiles("template.gotmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	tmpl.Execute(out, data)
+}
